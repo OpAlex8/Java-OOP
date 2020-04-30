57ecb70482a4e5ac551323906c63f1f275a288a2
OpAlex8 <38791984+OpAlex8@users.noreply.github.com>
2020-04-30 18:34:43 +0300

    Update AuditService.java

--- proiect PAO/src/com/company/AuditService.java
+++ proiect PAO/src/com/company/AuditService.java
@@ -13,18 +13,8 @@ public class AuditService {
     AuditService (String cale){
         this.path = cale;
     }
-    
-    void printRecords() throws IOException {
-        BufferedReader bufferRead = new BufferedReader(new FileReader(this.path));
-        String linie;
-        while((linie = bufferRead.readLine()) != null) {
-            String[] s = linie.split(",");
-            System.out.println(s[0] + ", " + s[1]);
-        }
-        bufferRead.close();
-    }
-    
-    void addRecord(String numeActiune) throws IOException {
+
+    void AdaugaAudit(String numeActiune) throws IOException {
         Date date= new Date();
         long time = date.getTime();
         Timestamp timpStamp = new Timestamp(time);
@@ -32,5 +22,15 @@ public class AuditService {
         FileWriter fileWrite = new FileWriter(this.path , true);
         fileWrite.write(numeActiune + ',' + timpString + "\n");
         fileWrite.close();
+    }
+    
+    void AfiseazaAudit() throws IOException {
+        BufferedReader bufferRead = new BufferedReader(new FileReader(this.path));
+        String linie;
+        while((linie = bufferRead.readLine()) != null) {
+            String[] s = linie.split(",");
+            System.out.println(s[0] + ", " + s[1]);
+        }
+        bufferRead.close();
     }
 }
