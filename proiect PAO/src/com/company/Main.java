7891e51ad804f8c664b9b79fae72036223cd0abd
OpAlex8 <38791984+OpAlex8@users.noreply.github.com>
2020-04-30 18:22:35 +0300

    Update Main.java

--- proiect PAO/src/com/company/Main.java
+++ proiect PAO/src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
-import com.company.Serviciu;
-
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +13,7 @@ public class Main {
         System.out.println("7. Programeaza activitate noua.");
         System.out.println("8. Verifica disponibilitatea unui angajat la un anumit moment.");
         System.out.println("9. Afiseaza sedintele departamentelor.");
+        System.out.println("10. Afiseaza auditul.");
         System.out.println("11. Exit");
         System.out.println();
         System.out.println("Optiunea dumneavoastra:");
@@ -27,28 +27,64 @@ public class Main {
         int optiune;
         Serviciu serviciu = new Serviciu();
         while (true) {
+            AuditService audit = new AuditService("AuditLog.csv");
             AfiseazaMeniu();
             optiune = in.nextInt();
             if (optiune==1) {
                 serviciu.AfiseazaDepartamente();
+                try {
+                    audit.addRecord("Afisare Departament");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             } else if (optiune==2) {
                 serviciu.CitesteDepartamentDeLaTastatura();
                 System.out.print("Departamentul a fost adaugat!");
+                try {
+                    audit.addRecord("Adaugare departament nou");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }else if(optiune==3){
                 serviciu.AfiseazaAngajati();
+                try {
+                    audit.addRecord("Afisare angajati");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }else if(optiune==4){
                 serviciu.CitesteAngajatDeLaTastatura();
                 System.out.print("Angajatul a fost adaugat!");
+                try {
+                    audit.addRecord("Angajat nou adaugat");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }else if(optiune==5){
                 serviciu.StergeAngajat();
+                try {
+                    audit.addRecord("Angajat sters");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }else if(optiune==6) {
                 serviciu.AfiseazaProgrameleAngajatilor();
+                try {
+                    audit.addRecord("Afisare program angajati");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }else if(optiune==7) {
                 System.out.println("Ce fel de activitate doriti sa programati? (1=activitate individuala/2=sedinta departament)");
                 int tip;
                 tip = in.nextInt();
                 if (tip == 1) {
                     serviciu.ProgrameazaActivitateNoua();
+                    try {
+                        audit.addRecord("Activitate angajat adaugata");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 } else if (tip == 2) {
                     System.out.println("Nu am mai avut timp sa implementez aceasta optiune");
                     // serviciu.ProgrameazaSedintaNoua();
@@ -58,7 +94,11 @@ public class Main {
             }else if(optiune==9) {
                 System.out.println("Nu am mai avut timp sa implementez aceasta optiune");
             }else if(optiune==10) {
-                System.out.println("Nu am mai avut timp sa implementez aceasta optiune");
+                try {
+                    audit.printRecords();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }else if(optiune==11) {
                 System.out.print("O zi buna!");
                 return;
